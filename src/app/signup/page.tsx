7365da046501012f36b09b1684d350e286934b60
camyacodes <camyacodes@gmail.com>
2023-11-10 03:59:03 -0500

    successfully connect db and added new user signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,27 +1,49 @@
 "use client";
 import Link from "next/link";
-import React from "react"
+import React, { useEffect } from "react"
 import { useRouter } from "next/navigation";
-// import {axios} from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 
 
 export default function SignupPage() {
+    const router = useRouter();
+    const [loading, setLoading] = React.useState(false)
     const [user, setUser] = React.useState({
         email: "",
         password: "",
         username: "",
     })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
 
     const onSignup = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user)
+            console.log("signup success", response.data)
+            router.push("/login")
+        } catch (error:any) {
+            console.log("Signup failed", error.message)
 
+            toast.error(error.message)
+        } finally {
+            setLoading(false);
+        }
     }
+
+    useEffect(() => {
+        if(user.email.length > 0 && user.username.length > 0 && user.password.length > 0)
+        {setButtonDisabled(false) } else {
+            setButtonDisabled(true)
+        }
+    }, [user]);
 
     return (
         <div className="bg-grey-lighter min-h-screen flex flex-col">
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                 <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                    <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+                    <h1 className="mb-8 text-3xl text-center">{loading ? "Creating Account..." : "Sign up"}</h1>
                     <input 
                         id="username"
                         className="block border border-grey-light w-full p-3 rounded mb-4"
@@ -52,7 +74,7 @@ export default function SignupPage() {
                         onClick={onSignup}
                         type="submit"
                         className="w-full text-center py-3 rounded bg-green-500 text-black hover:bg-green-600 focus:outline-none my-1"
-                    >Create Account</button>
+                    >{buttonDisabled ? "Cannot Signup" : "Create Account"} </button>
 
                 </div>
 
